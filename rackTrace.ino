bfd30f2d4453733a08ce98a76ee8c3555b2613f0
Dhruv Jagga <31867591+jaggadhruv@users.noreply.github.com>
2021-05-28 12:02:35 +0200

    version20210528

--- rackTrace.ino
+++ rackTrace.ino
@@ -1,27 +1,24 @@
 #include <ArduinoBLE.h>                 // Include the Arduino BLE Library       
-#include <Arduino_LSM9DS1.h>            // Using IMU Sensor <Accelerometer, Gyroscope and Magneto meter>
-                                        // 
-
-static const char* hello = "Hello World!";
+#include <Arduino_LSM9DS1.h>            // Using IMU Sensor <Accelerometer, Gyroscope and Magneto meter>                   
 
 BLEService arduinoService("1101");  // User defined service
 
-BLEStringCharacteristic helloCharacteristic("00002A3D-0000-1000-8000-00805f9b34fb", BLERead | BLENotify | BLEBroadcast ,15);
-BLEIntCharacteristic RSSICharacteristic( "0000A666-0000-1000-8000-00805f9b34fb", BLERead | BLENotify );
+BLECharacteristic macCharacteristic("00002A3D-0000-1000-8000-00805f9b34fb", BLERead | BLENotify | BLEBroadcast ,256, false);
+BLEByteCharacteristic RSSICharacteristic( "0000A666-0000-1000-8000-00805f9b34fb", BLERead | BLENotify );
 BLEUnsignedCharCharacteristic batteryLevelCharacteristic("00002101-0000-1000-8000-00805f9b34fb", BLERead | BLENotify);
+BLEBoolCharacteristic isMovingCharacteristic( "0000C666-0000-1000-8000-00805f9b34fb", BLERead | BLENotify );
+
+#define NUMBER_OF_REFERENCE_POINTS 3
+BLECharacteristic rssiDistancesCharacteristic( "0000B666-0000-1000-8000-00805f9b34fb", BLERead | BLENotify, NUMBER_OF_REFERENCE_POINTS * sizeof( float ) );
 
 unsigned batteryLevel = 0;
 int rssiValue = 0;
 
+String referencePointsNames[NUMBER_OF_REFERENCE_POINTS] = { "", "", "" };
+float referencePointsDistances[NUMBER_OF_REFERENCE_POINTS] = { 0, 0, 0 };
+
 // Sleep Mode variables
 const long interval = 1000 * 20;         // interval at which to sleep (milliseconds)
-
-// Update - Dhruv (17-03-2021)
-// Structure to store two different types as return from the SleepMode and ActiveMode functions
-struct ArduinoStatus{
-  volatile int ArduinoMode;
-  volatile int inMotion;  
-};
 
 // Variable definition for the logic to check if the arduino is moving or not
 float x, y, z;
@@ -32,8 +29,6 @@ bool dx = false;
 bool dy = false;
 bool dz = false;
 
-struct ArduinoStatus status;
-
 bool inMotion = false;
 float sensitivity = 0.008;
 int ArduinoMode = 0;
@@ -43,40 +38,90 @@ unsigned long diffMillis = 0;
 unsigned long currentMillis = 0;
 unsigned long startMillis = 0;
 
-/*
- * LEDS
- */
-//void connectedLight() 
-//{
-//  digitalWrite(LEDR, HIGH);
-//  digitalWrite(LEDG, LOW);
-//}
-//
-//void disconnectedLight() 
-//{
-//  digitalWrite(LEDR, LOW);
-//  digitalWrite(LEDG, HIGH);
-//}
-//
+// Variable declaration for the Blink Timer in Sleep Mode
+unsigned long startBlinkMillis = 0;
+unsigned long currentBlinkMillis = 0;
+const unsigned long blinkPeriod = 1000 * 20;
+
+unsigned long diffMillis2 = 0;
+unsigned long currentMillis2 = 0;
+unsigned long currentMillis3 = 0;
+
+//-----------------------------------------------
+
+/**
+* @brief Structure to store two different types as return from the SleepMode and ActiveMode functions
+* @author Dhruv Jagga
+*/
+struct ArduinoStatus{
+  volatile int ArduinoMode;
+  volatile int inMotion;  
+};
+
+struct ArduinoStatus status;
+
+//-----------------------------------------------
+
+/**
+* @brief This method set the LED to green
+* @author Ákos Gréczy
+*/
+void connectedLight() 
+{
+  digitalWrite(LEDR, HIGH);
+  digitalWrite(LEDG, LOW);
+}
+
+//-----------------------------------------------
+
+/**
+* @brief This method set the LED to red
+* @author Ákos Gréczy
+*/
+void disconnectedLight() 
+{
+  digitalWrite(LEDR, LOW);
+  digitalWrite(LEDG, HIGH);
+}
+
+//-----------------------------------------------
+
+/**
+* @brief This method initialize the serial communication
+* @author Ákos Gréczy
+*/
 void initSerialCommunication()
 {
   Serial.begin(9600);    // initialize serial communication
   while (!Serial);
 }
 
+//-----------------------------------------------
+
+/**
+* @brief This method setup the arduino service
+* @author Ákos Gréczy
+*/
 void setupBLEService()
 {
   BLE.setLocalName("Nano33BLE");  // Set name for connection
   BLE.setAdvertisedService(arduinoService); // Advertise service
-  arduinoService.addCharacteristic(helloCharacteristic); // Add characteristic to service
+  
+  arduinoService.addCharacteristic(macCharacteristic);
   arduinoService.addCharacteristic(RSSICharacteristic);
   arduinoService.addCharacteristic(batteryLevelCharacteristic);
+  arduinoService.addCharacteristic(rssiDistancesCharacteristic);
+  arduinoService.addCharacteristic(isMovingCharacteristic);
+  
   BLE.addService(arduinoService); // Add service
 }
 
-//---------------------------------------------
-// Update - Dhruv (17-03-2021)
-// Setting up service for the IMU
+//-----------------------------------------------
+
+/**
+* @brief This method setup the IMU service
+* @author Dhruv Jagga
+*/
 void setupIMUService() {
   if (!IMU.begin()) {
     Serial.println("Failed to initialize IMU!");
@@ -93,19 +138,69 @@ void setupIMUService() {
 
 //---------------------------------------------
 
+/**
+* @brief This method calculates the RSSI distance from the reference points. 
+* The function search for "ReferencePoint" string. 
+* If the nearby devices has this name, the function calculate the distance and print it to the serial console
+* @author Ákos Gréczy
+*/
+void calculateRSSIBasedOnFixPoints()
+{
+  BLEDevice peripheral = BLE.available();
+  
+  if (peripheral) 
+  {
+    if (peripheral.hasLocalName())
+    {
+      //if( peripheral.localName() == "ReferencePoint" )
+      if( peripheral.localName() == "JBL Charge 4" )
+      {
+        Serial.println();
+        Serial.print( "Local name: " );
+        Serial.print( peripheral.localName() );
+        Serial.print( "\t rssi:" );
+        Serial.println( abs( peripheral.rssi() - BLE.rssi() ) );
+      }
+    }
+  }
+}
+
+//-----------------------------------------------
+
+/**
+* @brief This method calculates the battery level of the arduino
+* @author Ákos Gréczy
+*/
+void batteryLevelCalculation()
+{
+  int battery = analogRead(A0);
+  unsigned tempBatteryLevel = map(battery, 0, 1023, 0, 100);
+  if( tempBatteryLevel != batteryLevel )
+  {
+    batteryLevel = tempBatteryLevel;
+    batteryLevelCharacteristic.writeValue(batteryLevel);
+  }
+}
+
+//-----------------------------------------------
+
 void setup() 
 {
   initSerialCommunication();  
 
-//  disconnectedLight();//pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin
+  disconnectedLight();//pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin
 
   if (!BLE.begin()) {   // initialize BLE
     Serial.println("starting BLE failed!");
     while (1);
   }
+  
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
 
   setupBLEService();
-  setupIMUService();
 
   BLE.advertise();  // Start advertising
   Serial.print("Peripheral device MAC: ");
@@ -113,10 +208,13 @@ void setup()
   Serial.println("Waiting for connections...");
 }
 
+
 //---------------------------------------------
 
-// Function definition of IMU reading (Accelerometer)
-
+/**
+* @brief Function definition of IMU reading (Accelerometer)
+* @author Dhruv Jagga
+*/
 void readAccelerometerData(){
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
@@ -128,6 +226,12 @@ void readAccelerometerData(){
   }
 }
 
+//---------------------------------------------
+
+/**
+* @brief Timer to switch to other state
+* @author Dhruv Jagga
+*/
 bool update_timer(){
   diffMillis = currentMillis - startMillis;
   if(diffMillis >= interval){
@@ -137,18 +241,31 @@ bool update_timer(){
   }
 }
 
+bool update_timer2(){
+  diffMillis2 = currentBlinkMillis - startBlinkMillis;
+  if(diffMillis2 >= blinkPeriod){
+    return true;
+  }else{
+    return false;
+  }
+}
+
 //---------------------------------------------
 
-// Function returning structure - Active Mode functionality, everything active on Arduino
-// Logic for arduino moving is also called in the function
+/**
+* @brief Function returning structure - Active Mode functionality, everything active on Arduino
+*        Logic for arduino moving is also called in the function
+* @author Dhruv Jagga
+*/
 struct ArduinoStatus ActiveMode() {
 
-  // Return: ArduinoMode, inMotion
-  //           Arduino Mode - To know the exact mode in which arduino is
-  //           Arduino Mode 1 - means arduino is in active mode
-  //           inMotion - To know if arduino moving or not based on IMU accelerometer readings 
-  //           (if it is changing continuously it is in moving state (1))
-
+  /** Return: ArduinoMode, inMotion
+  *           Arduino Mode - To know the exact mode in which arduino is
+  *           Arduino Mode 1 - means arduino is in active mode
+  *           inMotion - To know if arduino moving or not based on IMU accelerometer readings 
+  *           (if it is changing continuously it is in moving state (1))
+  */
+  
   struct ArduinoStatus aStatus;
   
   digitalWrite(LED_PWR, HIGH); // turn power LED
@@ -163,18 +280,15 @@ struct ArduinoStatus ActiveMode() {
   if (dx || dy || dz){
     // Start timer when the arduino stops moving
     currentMillis = millis();
-//    Serial.println(currentMillis);    
     inMotion = false;
     xPrev = x;
     yPrev = y;
     zPrev = z;
-//    ArduinoMode = 0;
   } else {
     inMotion = true;
     xPrev = x;
     yPrev = y;
     zPrev = z;
-//    ArduinoMode = 1;
   }
   
   Serial.print("Active Mode");
@@ -188,7 +302,16 @@ struct ArduinoStatus ActiveMode() {
   Serial.print(inMotion);
   Serial.print('\t');
   Serial.println(ArduinoMode);
-  delay(1000);                          // wait to send limited data                       
+  delay(1000);                          // wait to send limited data 
+
+  //This part just simply advertise the RSSI of arduino
+  if( rssiValue != BLE.rssi() )
+  {
+    rssiValue = BLE.rssi();
+    RSSICharacteristic.setValue( rssiValue );  
+  }
+
+  batteryLevelCalculation();
 
   ArduinoMode = 1;
   aStatus.ArduinoMode = ArduinoMode;
@@ -197,16 +320,24 @@ struct ArduinoStatus ActiveMode() {
  return aStatus;
 }
 
-// Update - Dhruv (24-03-2021)
-// Setting up service for the Sleep Mode
+//---------------------------------------------
+
+/**
+* @brief Setting up service for the Sleep Mode
+* @author Dhruv Jagga
+*/
+
 struct ArduinoStatus SleepMode(){
 
-  // Return: ArduinoMode, inMotion
-  //           Arduino Mode - To know the exact mode in which arduino is
-  //           Arduino Mode 0 - means arduino is in sleep mode
-  //           inMotion - To know if arduino moving or not based on IMU accelerometer readings 
-  //           (if it is not changing continuously it is in still state (0))
+  /** Return: ArduinoMode, inMotion
+  *           Arduino Mode - To know the exact mode in which arduino is
+  *           Arduino Mode 0 - means arduino is in sleep mode
+  *           inMotion - To know if arduino moving or not based on IMU accelerometer readings 
+  *           (if it is not changing continuously it is in still state (0))
+  */
+    
   
+    currentBlinkMillis = millis();    
     struct ArduinoStatus aStatus;
     
     digitalWrite(LED_PWR, LOW); // turn off power LED
@@ -222,15 +353,12 @@ struct ArduinoStatus SleepMode(){
       xPrev = x;
       yPrev = y;
       zPrev = z;
-//      ArduinoMode = 0;
     } else {
       inMotion = true;
       xPrev = x;
       yPrev = y;
       zPrev = z;
-//      ArduinoMode = 1;
       startMillis = millis();
-      Serial.println(startMillis);
     }
      
     Serial.print("Sleep Mode");
@@ -243,92 +371,186 @@ struct ArduinoStatus SleepMode(){
     ArduinoMode = 0;
     aStatus.ArduinoMode = ArduinoMode;
     aStatus.inMotion = inMotion;
- 
+
  return aStatus;
 }
 
+//---------------------------------------------
+
+/**
+* @brief Setting up service for the Blink Mode
+* @author Dhruv Jagga
+*/
+
+struct ArduinoStatus BlinkMode(){
+  startBlinkMillis = millis();
+  
+  struct ArduinoStatus aStatusSleep;
+  
+  aStatusSleep = SleepMode();
+  ArduinoMode = 2;
+  inMotion = aStatusSleep.inMotion;
+
+  struct ArduinoStatus aStatus;
+
+  aStatus.ArduinoMode = ArduinoMode;
+  aStatus.inMotion = inMotion;
+    
+  Serial.println("Blink and Publish Data");
+
+  //This part just simply advertise the RSSI of arduino
+  if( rssiValue != BLE.rssi() )
+  {
+    rssiValue = BLE.rssi();
+    RSSICharacteristic.setValue( rssiValue );  
+  }
+
+  batteryLevelCalculation();
+  
+  return aStatus;
+}
+
+//---------------------------------------------
+
 // ________________Tests_________________
-// Test - 1 (Test for the timer)
+
+/**
+* @brief Test - 1 (Test for the timer)
+* @author Dhruv Jagga
+*/
 
 void millis_test(){
   unsigned long start_time = millis();
-  Serial.print("Start time for the timer test: ");
-  Serial.print('\t');
-  Serial.println(start_time);
+//  Serial.print("Start time for the timer test: ");
+//  Serial.print('\t');
+//  Serial.println(start_time);
 
   while(millis() - start_time < 10000){
     Serial.println(millis());
   }
 
   unsigned long end_time = millis();
-  Serial.print("End of test - time duration: ");
-  Serial.print('\t');
-  Serial.println(end_time - start_time);
+//  Serial.print("End of test - time duration: ");
+//  Serial.print('\t');
+//  Serial.println(end_time - start_time);
 
   if ((end_time - start_time) >= (10000 - 50) || (end_time - start_time) <= (10000 + 50)){
-    Serial.println("Test Passed");
+    Serial.println("Test-1 Passed");
   }else{
-    Serial.println("Test Failed");
+    Serial.println("Test-1 Failed");
   }
 }
 
+//---------------------------------------------
+
+/**
+* @brief Test - 2 (Test for the Arduino Mode)
+* @author Dhruv Jagga
+*/
+
+void arduinoStatus_test(){
+  struct ArduinoStatus aStatusActive;
+  struct ArduinoStatus aStatusSleep;
+  struct ArduinoStatus aStatusBlink;
+
+  aStatusActive = ActiveMode();
+  aStatusSleep = SleepMode();
+  aStatusBlink = BlinkMode();
+
+  if (aStatusActive.ArduinoMode == 1 && aStatusSleep.ArduinoMode == 0 && aStatusBlink.ArduinoMode == 2){
+    Serial.println("Test-2 Passed");
+  }else{
+    Serial.println("Test-2 Failed");
+  }
+}
+
+//---------------------------------------------
+
+/**
+* @brief Test - 3 (Test for the Arduino Motion Detection)
+* @author Dhruv Jagga
+*/
+
+void arduinoMotion_test(){
+  struct ArduinoStatus aStatusActive;
+  struct ArduinoStatus aStatusSleep;
+  struct ArduinoStatus aStatusBlink;
+
+  aStatusActive = ActiveMode();
+  aStatusSleep = SleepMode();
+  aStatusBlink = BlinkMode();
+
+  if (aStatusActive.inMotion == 1 || aStatusActive.inMotion == 0 &&
+      aStatusSleep.inMotion == 1 || aStatusSleep.inMotion == 0 &&
+      aStatusBlink.inMotion == 1 || aStatusBlink.inMotion == 0){
+    Serial.println("Test-3 Passed");
+  }else{
+    Serial.println("Test-3 Failed");
+  }
+}
+
+//---------------------------------------------
+
+/**
+* @brief Routine to run the tests
+* @author Dhruv Jagga
+*/
 
 void run_test(){
   millis_test();
+  arduinoStatus_test();
+  arduinoMotion_test();  
 }
+
+//---------------------------------------------
+
 void loop() 
 {
-//  BLEDevice central = BLE.central();  // Wait for a BLE central to connect
-//
-//  // if a central is connected to the peripheral:
-//  if (central) 
-//  {
-//    Serial.print("Connected to central MAC: ");
-//    Serial.println(central.address());
-//    connectedLight();
-//    
-//    while (central.connected())
-//    {
-//      helloCharacteristic.setValue(hello); // Set greeting string
-//
-//      if( rssiValue != BLE.rssi() )
-//      {
-//        rssiValue = BLE.rssi();
-//        RSSICharacteristic.setValue( rssiValue );  
-//      }
-//
-//      {//Battery level calculation
-//        int battery = analogRead(A0);
-//        unsigned tempBatteryLevel = map(battery, 0, 1023, 0, 100);
-//        if( tempBatteryLevel != batteryLevel )
-//        {
-//          batteryLevel = tempBatteryLevel;
-//          batteryLevelCharacteristic.writeValue(batteryLevel);
-//          //Serial.print("Battery Level % is now: ");
-//          //Serial.println(batteryLevel);
-//        }
-//      }
-//    }
-//
-//    disconnectedLight();
-//    Serial.print("Disconnected from central MAC: ");
-//    Serial.println(central.address());
-//  }
+  BLEDevice central = BLE.central();  // Wait for a BLE central to connect
+  
+  // if a central is connected to the peripheral:
+  if (central) 
+  {
+    Serial.print("Connected to central MAC: ");
+    Serial.println(central.address());
+    Serial.println(central.deviceName());
+    //macCharacteristic.writeValue(BLE.address());
+    connectedLight();
+    
+    while (central.connected())
+    {
+      // Update - Dhruv (20/05/2021)
+      //calculateRSSIBasedOnFixPoints();
+
+      // Now if arduino is not moving for more than 20sec (lets say) and is in active mode, then put the arduino to sleep mode
+      if  (status.inMotion == false){       
+        if (update_timer()){
+          status = SleepMode();
+
+            if (update_timer2()){
+              status = BlinkMode();
+            }else{
+              status = SleepMode();
+            }          
+          }else {
+          status = ActiveMode();
+        }
+    }else{
+      status = ActiveMode(); 
+    }
+    }
+
+    disconnectedLight();
+    Serial.print("Disconnected from central MAC: ");
+    Serial.println(central.address());
+  }
+
 
 
   //---------------------------------------------
-  // Update - Dhruv (24/03/2021)
+  // Update - Dhruv (27/05/2021)
+    
 
-// Now if arduino is not moving for more than 20sec (lets say) and is in active mode, then put the arduino to sleep mode
-   if  (status.inMotion == false){       
-      if (update_timer()){
-        status = SleepMode();
-        }else {
-        status = ActiveMode();
-      }
-   }else{
-    status = ActiveMode(); 
-   }
-
+    
 //   run_test();
 }
